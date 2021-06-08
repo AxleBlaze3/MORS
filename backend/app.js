00450fab8dd56e46d3933095c020d889eee1051e
Rohan Jo <rohan54518@gmail.com>
2021-06-08 20:21:58 +0530

    updated city , currAqi (x2)

--- backend/app.js
+++ backend/app.js
@@ -22,22 +22,7 @@ app.get('/',(req,res)=>{
   return res.send("Hello")
 })
 
-// const mongoUri='mongodb+srv://Axle:passwoo3@cluster0.urzpd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-// mongose.connect(mongoUri,{
-//     useNewUrlParser:true,
-//     useCreateIndex:true,
-//     useUnifiedTopology: true 
-// })
-// mongose.connection.on("connected",()=>{
-//     console.log("Connected to Mongo")
-// })
-// mongose.connection.on('error',(err)=>{
-//     console.error("Error connecting to Mongo",err)
-// })
-/*app.listen(3000,()=>{
-    console.log("listening:3000")
-})
-*/
+
 const MONGO_URI = process.env.MONGO_URI
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
